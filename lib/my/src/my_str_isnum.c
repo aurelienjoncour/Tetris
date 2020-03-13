26ce220ac918c639bab0ac37606a0eb4250870fa
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-03-13 19:15:56 +0100

    Fix error in lib function my_str_isnum

--- lib/my/src/my_str_isnum.c
+++ lib/my/src/my_str_isnum.c
@@ -5,10 +5,13 @@
 ** is a string of digits
 */
 
+#include <stdlib.h>
 #include <stdbool.h>
 
 bool my_str_isnum(char const *str)
 {
+    if (str == NULL)
+        return false;
     for (int i = 0; str[i] != '\0'; i++)
         if (str[i] < '0' || str[i] > '9')
             return false;
