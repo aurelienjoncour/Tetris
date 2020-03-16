4a663cda4613464ba771fa902bea6f5556b91fc1
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-03-16 17:14:08 +0100

    fix(src/tetris.c) adding missing ; l15

--- src/tetris.c
+++ src/tetris.c
@@ -12,6 +12,6 @@ int tetris(int argc, char **argv)
     flags_t *flags = get_flags(argc, argv);
 
     if (flags == NULL)
-        return EXIT_ERROR
+        return EXIT_ERROR;
     return EXIT_SUCCESS;
 }
