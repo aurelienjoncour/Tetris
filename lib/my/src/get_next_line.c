38bebd1bc78ecab44b16e401667827c4f0802e03
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-03-17 17:39:35 +0100

    fix(get_next_line.c) add get_next_line aurelien

--- lib/my/src/get_next_line.c
+++ lib/my/src/get_next_line.c
@@ -1,95 +1,88 @@
 /*
 ** EPITECH PROJECT, 2019
-** get_next_line
+** CPE_getnextline_2019
 ** File description:
-** get the file next line
+** get_next_line
 */
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <unistd.h>
 #include "my.h"
+#include <stdio.h>
 
-static char *read_file(int fd, char **too_much, int *size);
-
-static char *check_line(int fd, char *buffer, char **too_much, int *size)
-{
-    int i = my_strlen(*too_much);
-
-    for (; buffer[i] != '\n' && buffer[i] != '\0'
-    && i + 1 < READ_SIZE + my_strlen(*too_much); i++);
-    if (buffer[i] == '\n') {
-        free(*too_much);
-        buffer[i] = '\0';
-        *too_much = my_strdup(buffer + i + 1);
-    } else if (buffer[i] != '\0') {
-        buffer[i + 1] = '\0';
-        free(*too_much);
-        *too_much = buffer;
-        return read_file(fd, too_much, size);
-    } else {
-        free(*too_much);
-        *too_much = NULL;
-    }
-    return buffer;
-}
-
-static char *read_file(int fd, char **too_much, int *size)
-{
-    int much_len = my_strlen(*too_much);
-    char *buffer = malloc(sizeof(char) * (much_len + READ_SIZE + 1));
-    *size = buffer != NULL ? read(fd, buffer + much_len, READ_SIZE) : -1;
-
-    if (buffer == NULL)
-        return NULL;
-    if (*size <= 0) {
-        free(buffer);
-        buffer = NULL;
-        if (*too_much != NULL)
-            buffer = my_strdup(*too_much);
-        free(*too_much);
-        *too_much = NULL;
-        return buffer;
-    }
-    (buffer + much_len)[*size] = '\0';
-    for (int k = 0; *too_much != NULL && (*too_much)[k] != '\0'; k++)
-        buffer[k] = (*too_much)[k];
-    return check_line(fd, buffer, too_much, size);
-}
-
-static void fill_too_much(char **result, char **too_much, char *temp)
+static int my_strcpylen(char *dest, char const *src, bool c)
 {
     int i = 0;
 
-    if ((*too_much) != NULL && (*too_much)[0] == '\0')
-        *too_much = NULL;
-    for (; (*too_much) != NULL
-    && (*too_much)[i] != '\n' && (*too_much)[i] != '\0'; i++);
-    if ((*too_much) != NULL && (*too_much)[i] == '\n') {
-        *result = my_strndup((*too_much), i);
-        (*too_much) = my_strdup((*too_much) + i + 1);
-        free(temp);
+    if (c) {
+        if (dest == NULL)
+            return 0;
+        for (i = 0; src[i] != '\0'; i++)
+            dest[i] = src[i];
+        dest[i] = '\0';
+        return i;
+    }
+    else {
+        for (i = 0; dest[i] != '\0'; i++);
+        return i;
+    }
+}
+
+static char *my_realloc(char *src, size_t bytes)
+{
+    char *str = malloc(sizeof(char) * bytes);
+
+    if (str == NULL)
+        return NULL;
+    my_strcpylen(str, src, true);
+    free(src);
+    return str;
+}
+
+static int check_end_line(char const *str)
+{
+    for (int i = 0; str[i] != '\0'; i++)
+        if (str[i] == '\n')
+            return i;
+    return -1;
+}
+
+static char *return_str(char *str, char *tampon)
+{
+    if (check_end_line(str) > -1) {
+        my_strcpylen(tampon, str+check_end_line(str)+1, true);
+        str[check_end_line(str)] = '\0';
+        return str;
+    }
+    else {
+        if (my_strcpylen(str, NULL, false) == 0) {
+            free(str);
+            return NULL;
+        }
+        else
+            return str;
     }
 }
 
 char *get_next_line(int fd)
 {
-    static char *too_much = NULL;
-    static bool end_of_file = false;
-    int size;
-    char *result = NULL;
-    char *temp = too_much;
+    static char tampon[READ_SIZE] = {0};
+    char *str = malloc(READ_SIZE + my_strcpylen(tampon, NULL, 0) + 1);
+    int len = my_strcpylen(str, tampon, true);
 
-    fill_too_much(&result, &too_much, temp);
-    if (end_of_file) {
-        end_of_file = false;
+    tampon[0] = '\0';
+    if (fd == -1 || str == NULL)
         return NULL;
+    for (int n = 0; n >= 0;) {
+        if (check_end_line(str) == -1) {
+            n = read(fd, str+len, READ_SIZE);
+            len += n;
+        }
+        str[len] = '\0';
+        if (check_end_line(str) > -1 || n < READ_SIZE)
+            return return_str(str, tampon);
+        str = my_realloc(str, my_strcpylen(str, NULL, false) + READ_SIZE + 1);
     }
-    temp = read_file(fd, &too_much, &size);
-    end_of_file = size != READ_SIZE;
-    if (end_of_file) {
-        free(too_much);
-        too_much = NULL;
-    }
-    return temp;
+    return NULL;
 }
