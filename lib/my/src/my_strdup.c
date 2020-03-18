93936f65059404acad0b2824404b22b6f64897f2
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-03-18 19:55:29 +0100

    Fix lib my_strdup

--- lib/my/src/my_strdup.c
+++ lib/my/src/my_strdup.c
@@ -12,13 +12,14 @@ int my_strlen(char const *str);
 char *my_strdup(char const *src)
 {
     char *dest;
+    int const size = my_strlen(src);
 
     if (src == NULL)
         return NULL;
     dest = malloc(sizeof(char) * (my_strlen(src) + 1));
     if (dest == NULL)
         return NULL;
-    for (int i = 0; i <= my_strlen(src); i++)
+    for (int i = 0; i <= size; i++)
         dest[i] = src[i];
     return dest;
 }
