fc1fea3bd937a66531be6070046968f7b0481e7a
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-03-19 14:06:38 +0100

    Add free tetriminos

--- src/tetris.c
+++ src/tetris.c
@@ -41,6 +41,7 @@ int tetris(int argc, char **argv, char **env)
     if (exit_value != EXIT_SUCCESS)
         return exit_value;
     free_flags_struct(game.flag);
+    destroy_tetriminos(game.tetriminos, game.nb_tetriminos);
     ioctl(0, TCSETS, &term_backup);
     return exit_value;
 }
