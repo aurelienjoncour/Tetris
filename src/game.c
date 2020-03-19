13ee5e29ccbcc8176ed245b2d4e7efdd492369b3
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-03-19 16:48:04 +0100

    Add clock

--- src/game.c
+++ src/game.c
@@ -5,7 +5,13 @@
 ** tetris game
 */
 
+#include <time.h>
 #include "tetris.h"
+
+static bool is_lose(void)
+{
+    return 0;
+}
 
 static void print_board(game_t *game)
 {
@@ -31,6 +37,19 @@ static void print_board(game_t *game)
     refresh();
 }
 
+static int update_clock(clock_t *time, game_t *game)
+{
+    clock_t current = clock();
+
+    if (*time == -1 || current == -1)
+        return EXIT_ERROR;
+    if (current / CLOCKS_PER_SEC - *time / CLOCKS_PER_SEC > 1) {
+        print_board(game);
+        *time = clock();
+    }
+    return EXIT_SUCCESS;
+}
+
 int play_game(game_t *game)
 {
     clock_t time = clock();
@@ -38,6 +57,11 @@ int play_game(game_t *game)
     if (init_boards(game) == EXIT_ERROR || init_ncurses() == EXIT_ERROR)
         return EXIT_ERROR;
     print_board(game);
+    while (!is_lose()) {
+
+        if (update_clock(&time, game) == EXIT_ERROR)
+            return EXIT_ERROR;
+    }
     clear();
     endwin();
     return EXIT_SUCCESS;
