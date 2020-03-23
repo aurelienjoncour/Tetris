82021361c49519d77e31fd689892ad1b373251a0
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-03-23 10:27:40 +0100

    feat(lib/my/src/my_strcmp.c) fix strcmp

--- lib/my/src/my_strcmp.c
+++ lib/my/src/my_strcmp.c
@@ -6,7 +6,7 @@
 */
 
 #include <stdlib.h>
-
+/*
 int my_strcmp(char const *s1, char const *s2)
 {
     if (s1 == NULL || s2 == NULL)
@@ -22,4 +22,20 @@ int my_strcmp(char const *s1, char const *s2)
             return 1;
     }
     return 0;
+}*/
+int my_strlen(char *str);
+
+int my_strcmp(char const *s1, char const *s2)
+{
+    int i = 0;
+
+    while (s1[i] != '\0' || s2[i] != '\0') {
+        if (s1[i] < s2[i])
+            return (s1[i] - s2[i]);
+        else if (s1[i] > s2[i])
+            return (s1[i] - s2[i]);
+        else
+            i++;
+    }
+    return (0);
 }
