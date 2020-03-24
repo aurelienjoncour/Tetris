c6639168542f9194a84c4452ac1594b6267eca36
Vincent Andrieu <vincent.andrieu@epitech.eu>
2020-03-24 15:27:31 +0100

    Fix code and remove comments

--- lib/my/src/my_strcmp.c
+++ lib/my/src/my_strcmp.c
@@ -6,36 +6,16 @@
 */
 
 #include <stdlib.h>
-/*
+#include "my.h"
+
 int my_strcmp(char const *s1, char const *s2)
 {
-    if (s1 == NULL || s2 == NULL)
+    if (s1 == NULL  || s2 == NULL)
         return 84;
-    for (int i = 0; s1[i] != '\0' && s2[i] != '\0'; i++) {
-        if (s1[i] > s2[i])
-            return 1;
-        if (s1[i] < s2[i])
-            return -1;
-        if (s1[i] == s2[i] && s1[i + 1] == '\0' && s2[i + 1] != '\0')
-            return -1;
-        if (s1[i] == s2[i] && s2[i + 1] == '\0' && s1[i + 1] != '\0')
-            return 1;
-    }
-    return 0;
-}*/
-int my_strlen(char *str);
-
-int my_strcmp(char const *s1, char const *s2)
-{
-    int i = 0;
-
-    while (s1[i] != '\0' || s2[i] != '\0') {
+    for (int i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
         if (s1[i] < s2[i])
             return (s1[i] - s2[i]);
         else if (s1[i] > s2[i])
             return (s1[i] - s2[i]);
-        else
-            i++;
-    }
-    return (0);
+    return 0;
 }
